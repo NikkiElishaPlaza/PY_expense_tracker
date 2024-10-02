47cf6a23609055e515233a5608392ddb8a5df305
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-03 00:26:26 +0800

    Added categories and how the user will input their chosen category

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -35,10 +35,41 @@ print(f"\n{separator}\n")
 
 while True:
     try:
-        expense_amount = floar(input("Enter expense amount in PHP: "))
+        expense_amount = float(input("Enter expense amount in PHP: "))
         break
     except ValueError:
         print("Kindly enter a valid number for the expense amount.")
+
+
+#List categories
+    expense_categories = [
+        "Food"
+        "Home"
+        "Work"
+        "Fun"
+        "Miscellaneous"
+    ]
+
+print(f"\n{separator}\n")
+
+while True:
+    print("Select a category: ")
+    for i category_name in enumerate(expense_categories):
+    print(f"\n {i + 1}/ {category_name}\n")
+    
+    value_range = f"[1 - {len(expense_categories)}]"
+
+    try:
+        selected_index = int(input(f"{separator}\n\n Enter a category number {value_range}: "))-1
+
+        if selected_index in range(len(expense_categories)):
+            selected_category = expense_categories[selected_index]
+            new_expense = Expense(name = expense_name, category = selected_category, amount= expense_amount)
+        else:
+            print("Invalid category. Please try again.")
+
+    except ValueError:
+        print(f"Kindly enter a valid category between 1 and {len(expense_categories)}.")
 
 
 
