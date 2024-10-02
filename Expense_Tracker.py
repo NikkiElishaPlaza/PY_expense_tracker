ff89670e610431ff4863b163e7dc0934f037c1d2
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-02 23:55:30 +0800

    Removed embedded repository PY_Expense_Tracker

---
+++ Expense_Tracker.py
@@ -0,0 +1,17 @@
+from Expense import Expense
+
+separator = "=" * 50
+
+def main():
+
+def get_use_expense():
+
+def save_expense_to_file():
+
+def summarize_expenses():
+
+def continue_input():
+
+
+if __name__ == "_main_":
+    main()
