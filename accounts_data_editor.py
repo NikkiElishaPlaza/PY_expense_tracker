9ce05b699e67b9c115365afd78e9d96c7179da77
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2025-01-28 05:35:31 +0800

    section to write data to a csv file

--- accounts_data_editor.py
+++ accounts_data_editor.py
@@ -22,3 +22,30 @@ class Accounts_Data_Editor:
     
     def getFieldName(self):
         return self.fieldname
+    
+# write data to csv file
+    def Add_Account(self, Name, Password, Birth_Date, Contact_Number, Address):
+        filename = self.filename
+        fieldname = self.fieldname
+
+        try:
+            account = {"Name": str(Name), 
+                       "Password": str(Password), 
+                       "Birth_Date": str(Birth_Date), 
+                       "Contact_Number": str(Contact_Number), 
+                       "Address": str(Address)}
+
+            # check if the file exists and is not empty
+            file_exists = os.path.exists(filename) and os.path.getsize(filename) > 0
+
+            # create Financials CSV file
+            self.create_financial_files(self.filename)
+
+            return True
+        except Exception as e:
+
+            # error handling
+            print(f"Error: {e}")
+
+            # call return if there is an error to stop the process
+            return
