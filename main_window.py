88c7b730316a9bd1cdcfc3ac5678949e8a5866f1
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2025-01-28 05:13:37 +0800

    adding import statements and modules

---
+++ main_window.py
@@ -0,0 +1,8 @@
+import sys, csv, os, re
+from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PySide6.QtGui import QStandardItemModel, QStandardItem
+from PySide6.QtCore import Qt, QDate
+
+from accounts_data_editor import accounts_data_editor
+from financials_editor import financials_editor
+
